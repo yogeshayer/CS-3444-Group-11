b995f6ce36fdbfae0f565c16be6e11933066d046
Yogesh Ayer <yogeshayer@my.unt.edu>
2025-07-19 18:37:25 -0500

    changed

--- lib/db.ts
+++ lib/db.ts
@@ -1,7 +1,6 @@
-// lib/db.ts
-import { createClient } from "@datastax/astra-db-ts";
+import { AstraDB } from "@datastax/astra-db-ts";
 
-export const db = createClient({
+export const db = new AstraDB({
   endpoint: process.env.ASTRA_DB_API_ENDPOINT!,
   token: process.env.ASTRA_DB_APPLICATION_TOKEN!,
 });
