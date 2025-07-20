67da2ac3eb51085525878c52dd67114ff060daa9
Yogesh Ayer <yogeshayer@my.unt.edu>
2025-07-19 20:15:27 -0500

    added

---
+++ server.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const connectToDB = require('./db');
+
+const app = express();
+
+app.get('/', async (req, res) => {
+  const db = await connectToDB();                  // connect to DB
+  const tasks = await db.collection('tasks').find().toArray();  // read from "tasks" collection
+  res.send(tasks);
+});
+
+app.listen(3000, () => {
+  console.log("Server running on http://localhost:3000");
+});
