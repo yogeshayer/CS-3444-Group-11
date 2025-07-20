4d261689ecdd0274f0ea8bc8fa55a26b179ecdd4
yogeshayer <yogeshayer@my.unt.edu>
2025-07-20 15:07:58 -0500

    Update db.ts

--- lib/db.ts
+++ lib/db.ts
@@ -1,25 +1,13 @@
-import { Db, MongoClient } from 'mongodb'
+import { Db, MongoClient } from 'mongodb';
 
-const uri = process.env.MONGODB_URI || "mongodb+srv://ayersunil018:W4adLEnO6fcjCXSt@cluster0.ahcmhct.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
-
-let client: MongoClient
-let db: Db
+const uri = process.env.MONGODB_URI!;
+let client: MongoClient;
+let db: Db;
 
 export async function connectToDatabase() {
-  if (db) {
-    return { client, db }
-  }
-
-  try {
-    client = new MongoClient(uri)
-    await client.connect()
-    db = client.db('ChoreBoard')
-    console.log('✅ Connected to MongoDB!')
-    return { client, db }
-  } catch (error) {
-    console.error('❌ MongoDB connection error:', error)
-    throw error
-  }
+  if (db) return { db };
+  client = new MongoClient(uri);
+  await client.connect();
+  db = client.db('ChoreBoard');
+  return { db };
 }
-
-export { db }
