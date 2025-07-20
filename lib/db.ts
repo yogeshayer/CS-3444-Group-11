0a5ce9dd88f40e7d21a6d0d6c632e54e2c059434
yogeshayer <yogeshayer@my.unt.edu>
2025-07-20 14:13:47 -0500

    Update db.ts

--- lib/db.ts
+++ lib/db.ts
@@ -1,6 +1,6 @@
 import { Db, MongoClient } from 'mongodb'
 
-const uri = process.env.MONGODB_URI || "mongodb+srv://sunilayer018:t2rPxxBklFTv9wsC@choreboard.jdwvnun.mongodb.net/?retryWrites=true&w=majority"
+const uri = process.env.MONGODB_URI || "mongodb+srv://ayersunil018:W4adLEnO6fcjCXSt@cluster0.ahcmhct.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
 
 let client: MongoClient
 let db: Db
@@ -13,7 +13,7 @@ export async function connectToDatabase() {
   try {
     client = new MongoClient(uri)
     await client.connect()
-    db = client.db('choreboarddb')
+    db = client.db('ChoreBoard')
     console.log('✅ Connected to MongoDB!')
     return { client, db }
   } catch (error) {
