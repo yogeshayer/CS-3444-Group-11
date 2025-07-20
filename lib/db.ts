29900c920902e85f5783d9affb0ed2b3fbea5e27
Yogesh Ayer <yogeshayer@my.unt.edu>
2025-07-19 19:04:01 -0500

    fixed

--- lib/db.ts
+++ lib/db.ts
@@ -1,6 +1,6 @@
-import { createClient } from "@datastax/astra-db-ts";
+import { AstraDB } from "@datastax/astra-db-ts";
 
-const client = createClient({
+export const client = new AstraDB({
   databaseId: process.env.ASTRA_DB_ID!,
   region: process.env.ASTRA_DB_REGION!,
   applicationToken: process.env.ASTRA_DB_APPLICATION_TOKEN!,
